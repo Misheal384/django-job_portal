4a441cb32f37132def4a3072f2e4a98caf1b49df
misheal <eversonmisheal2@gmail.com>
2024-02-14 11:01:49 +0000

    updated company form and urls.py

---
+++ company/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Company
+from .form import UpdateCompanyForm
+from users.models import User
+
+# updating company because the company is already created
+def update_company(request):
+    company = Company.objects.get(user=request.user)
+    if request.method == 'POST':
+        form = UpdateCompanyForm(request.POST, instance=company)
+        if form.is_valid():
+            var = form.save(commit=False)
+            user = User.objects.get(id=request.user)  # Use request.user.id
+            user.has_company = True
+            var.save()
+            user.save()
+            messages.info(request, 'Your company is active now. You can start creating job ads')
+            return redirect('dashboard')
+        else:
+            messages.warning(request, 'Something went wrong')
+    else:
+        form = UpdateCompanyForm(instance=company)
+        context = {'form':form}  
+        return render(request, 'company/update_company.html', context)  
+
+
+#view company details
+def company_details(request, pk): #pk is primarykey
+    company = Company.objects.get(pk=pk)
+    context = {'company':company}
+    return render(request, 'company/company_details.html', context)
--- django_project/urls.py
+++ django_project/urls.py
@@ -20,7 +20,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('users.urls')),
-     path('dashboard/', include('dashboard.urls'))
+    path('company/', include('company.urls')),
+    path('dashboard/', include('dashboard.urls'))
 ]
 
 
